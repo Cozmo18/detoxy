c5baa2d828c2a4c58d1a5bb292d0f38d8ae6883b
anitamaxvim <107803920+anitamaxvim@users.noreply.github.com>
2025-03-04 16:45:58 +0000

    add train_model notebook

--- src/toxy_bot/ml/data/pipeline.py
+++ src/toxy_bot/ml/data/pipeline.py
@@ -3,11 +3,11 @@ from toxy_bot.ml.data.download import download_dataset
 from toxy_bot.ml.data.preprocess import process_data
 
 
-def run_pipeline() -> None:
+def run_pipeline(val_size: float = 0.2, batch_size: int = 128) -> None:
     print("Running data pipeline...")
     download_dataset()
-    process_data()
-    convert_to_tf_datasets()
+    process_data(val_size=val_size)
+    convert_to_tf_datasets(batch_size=batch_size)
     print("Data pipeline completed.")
 
 
