81ca02fc1c3113263d352515aae38363f30c5008
anitamaxvim <107803920+anitamaxvim@users.noreply.github.com>
2025-04-01 16:13:01 +0100

    Remove GPU tokenizer transfer logic from AutoTokenizerDataModule in datamodule.py to streamline data processing.

--- toxy_bot/ml/datamodule.py
+++ toxy_bot/ml/datamodule.py
@@ -2,7 +2,6 @@ import os
 from datetime import datetime
 
 import lightning.pytorch as pl
-import torch
 from datasets import load_dataset
 from lightning.pytorch.utilities.types import EVAL_DATALOADERS, TRAIN_DATALOADERS
 from lightning_utilities.core.rank_zero import rank_zero_info
@@ -93,10 +92,6 @@ class AutoTokenizerDataModule(pl.LightningDataModule):
                 self.dataset_name, split=self.train_split, cache_dir=self.cache_dir
             )
             dataset = dataset.train_test_split(train_size=self.train_size)  # type: ignore
-
-            # Move tokenizer to GPU if available
-            if torch.cuda.is_available():
-                self.tokenizer.to(torch.device("cuda"))
 
             self.train_data = dataset["train"].map(
                 self.preprocess,
