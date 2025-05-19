3bdcffde02fba10ebcaafc7ac11ee37cb14542be
anitamaxvim <107803920+anitamaxvim@users.noreply.github.com>
2025-05-19 14:58:23 +0100

    add server logs

--- detoxy/ml/server.py
+++ detoxy/ml/server.py
@@ -1,9 +1,11 @@
 from pathlib import Path
+import time
+import os
 
 import litserve as ls
 from litserve import Request, Response
 
-from detoxy.ml.config import SERVER_CONFIG
+from detoxy.ml.config import SERVER_CONFIG, CONFIG
 from detoxy.ml.module import ToxicClassifier
 
 
@@ -23,16 +25,33 @@ class SimpleLitAPI(ls.LitAPI):
         return request["input"]
 
     async def predict(self, input: str):
-        return self.lit_module.predict_step(input)
+        t0 = time.time()
+        output = self.lit_module.predict_step(input)
+        t1 = time.time()
+        self.log("model_time", t1 - t0)
+        return output
 
     async def encode_response(self, output: dict) -> Response:
         return {"output": output}
-
+    
+    
+class SimpleLogger(ls.Logger):
+    def process(self, key, value):
+        print(f"Recieved {key} with value {value}", flush=True)
+        
+class FileLogger(ls.Logger):
+    def process(self, key, value):
+        log_filepath = os.path.join(CONFIG.log_dir, "server_logs.txt")
+        with open(log_filepath, "a+") as f:
+            f.write(f"{key}: {value:.1f}\n")
+        
 
 if __name__ == "__main__":
     api = SimpleLitAPI(enable_async=True)
+    loggers = [SimpleLogger(), FileLogger()]
     server = ls.LitServer(
         api, 
+        loggers=loggers,
         accelerator=SERVER_CONFIG.accelerator,
         devices=SERVER_CONFIG.devices,
         timeout=SERVER_CONFIG.timeout,
